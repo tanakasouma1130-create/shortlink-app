d7ed71c6456e45dac35791113dbd4660b92e221e
tanakasouma1130-create <tanakasouma1130@gmail.com>
2026-06-11 23:56:44 +0900

    Implement POST route for image upload

---
+++ app/api/create/route.js
@@ -0,0 +1,17 @@
+export async function POST(req) {
+
+  const body = await req.json();
+
+
+
+  return Response.json({
+
+    success: true,
+
+    image: body.image,
+
+    url: body.url
+
+  });
+
+}
