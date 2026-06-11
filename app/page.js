41bd087c814bc3c1bf1ffe9a6be39e39a43c4c4a
tanakasouma1130-create <tanakasouma1130@gmail.com>
2026-06-11 23:41:11 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,39 @@
 export default function Home() {
   return (
     <div style={{
+      maxWidth: "500px",
+      margin: "50px auto",
       padding: "20px",
       fontFamily: "sans-serif"
     }}>
-      <h1>短縮リンク作成</h1>
+      <h1>OGP短縮リンク作成</h1>
 
-      <p>
-        ここに画像URLと飛び先URLを入力できるようにする
-      </p>
+      <input
+        placeholder="画像URL"
+        style={{
+          width: "100%",
+          padding: "10px",
+          marginBottom: "10px"
+        }}
+      />
+
+      <input
+        placeholder="飛び先URL"
+        style={{
+          width: "100%",
+          padding: "10px",
+          marginBottom: "10px"
+        }}
+      />
+
+      <button
+        style={{
+          width: "100%",
+          padding: "12px"
+        }}
+      >
+        作成
+      </button>
     </div>
   );
 }
