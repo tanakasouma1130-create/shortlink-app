4aca18a00c7124fb69514a97d0a4fdde50e6300f
tanakasouma1130-create <tanakasouma1130@gmail.com>
2026-06-11 23:39:47 +0900

    Add Home component with basic layout

---
+++ app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: "20px",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>短縮リンク作成</h1>
+
+      <p>
+        ここに画像URLと飛び先URLを入力できるようにする
+      </p>
+    </div>
+  );
+}
