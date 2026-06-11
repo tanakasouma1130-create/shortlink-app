afaedd7b392cee32589beb9b31353193368fd9d5
tanakasouma1130-create <tanakasouma1130@gmail.com>
2026-06-12 01:30:07 +0900

    Update route.js

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,11 +1,14 @@
+import { kv } from "@vercel/kv";
+
 export async function POST(req) {
   const body = await req.json();
 
   const id = Math.random().toString(36).substring(2, 8);
 
-  return Response.json({
-    id,
+  await kv.set(`link:${id}`, {
     image: body.image,
     url: body.url
   });
+
+  return Response.json({ id });
 }
