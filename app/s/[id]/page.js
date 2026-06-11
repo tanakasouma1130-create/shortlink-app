0de81d6a180101d830a6e6bf2d87dcba54371e41
tanakasouma1130-create <tanakasouma1130@gmail.com>
2026-06-12 00:44:42 +0900

    Create page.js

---
+++ app/s/[id]/page.js
@@ -0,0 +1,11 @@
+export default function Page({ params }) {
+  return (
+    <html>
+      <head>
+        <title>詳細はこちら</title>
+        <meta httpEquiv="refresh" content="0;url=/" />
+      </head>
+      <body>移動中...</body>
+    </html>
+  );
+}
