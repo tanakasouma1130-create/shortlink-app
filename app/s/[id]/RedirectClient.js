151dec5c9e621ba6e75df4a9bcfa6623dcaf6514
tanakasouma1130-create <tanakasouma1130@gmail.com>
2026-06-12 10:34:39 +0900

    Update RedirectClient.js

--- app/s/[id]/RedirectClient.js
+++ app/s/[id]/RedirectClient.js
@@ -5,13 +5,18 @@ import { useEffect } from "react";
 export default function RedirectClient({ url }) {
   useEffect(() => {
     if (url) {
-      window.location.replace(url);
+      setTimeout(() => {
+        window.location.href = url;
+      }, 300);
     }
   }, [url]);
 
   return (
     <div style={{ background: "#000", color: "#000", height: "100vh" }}>
       移動中...
+      <a href={url} style={{ color: "#000" }}>
+        開く
+      </a>
     </div>
   );
 }
